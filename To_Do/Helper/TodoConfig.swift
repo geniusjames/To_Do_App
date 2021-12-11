1e51658ec38e2433027e8487bd8f4ac9f2f6bbcc
James Anyanwu <jameslecturer@yahoo.com>
2021-12-11 16:21:17 +0100

    Added Helper and Models

---
+++ To_Do/Helper/TodoConfig.swift
@@ -0,0 +1,21 @@
+//
+//  TodoConfig.swift
+//  To_Do
+//
+//  Created by Geniusjames on 11/12/2021.
+//
+
+import Foundation
+
+class TodoConfig{
+    func  createTask(){
+        
+    }
+    func deleteTask(){
+        
+    }
+    func updateTask(){
+        
+    }
+    
+}
---
+++ To_Do/Model/TodoModel.swift
@@ -0,0 +1,17 @@
+//
+//  TodoModel.swift
+//  To_Do
+//
+//  Created by Geniusjames on 11/12/2021.
+//
+
+import Foundation
+
+struct TodoModel: Codable{
+    let id: Int
+    var title: String
+    var description: String
+    var date: String
+    var time: String
+    var isDone: Bool
+}
