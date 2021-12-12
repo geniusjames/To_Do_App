01fdeb058401936aea9cd95b2621f66391d79d8a
James Anyanwu <jameslecturer@yahoo.com>
2021-12-12 10:37:53 +0100

    Created and fetched Todo

--- To_Do/Helper/TodoConfig.swift
+++ To_Do/Helper/TodoConfig.swift
@@ -8,7 +8,27 @@
 import Foundation
 
 class TodoConfig{
-    func  createTask(){
+    
+    let userDefault = UserDefaults.standard
+    var data: [Data]?
+    
+    init(){
+        guard let endcodedList = userDefault.object(forKey: "key") as? [Data] else {
+            data = []
+            return
+            }
+        data = endcodedList
+        }
+            
+    
+    func  createTask(task: TodoModel){
+        
+        let encoder = JSONEncoder()
+        if let encodedData = try? encoder.encode(task){
+            data?.append(encodedData)
+            userDefault.set(data, forKey: "key")
+            
+        }
         
     }
     func deleteTask(){
@@ -17,5 +37,17 @@ class TodoConfig{
     func updateTask(){
         
     }
-    
+    func fetchTask(){
+        if let savedTasksEncoded = userDefault.object(forKey: "key") as? [Data] {
+            let decoder = JSONDecoder()
+            for i in savedTasksEncoded{
+                if let decodedTask = try? decoder.decode(TodoModel.self, from: i) {
+                    print("reached")
+                    print(decodedTask)
+                    print(decodedTask.title)
+                }
+            }
+            
+        }
+    }
 }
--- To_Do/To_Do/View/onBoardingViewController.swift
+++ To_Do/To_Do/View/onBoardingViewController.swift
@@ -13,5 +13,12 @@ class onBoardingViewController: UIViewController {
         super.viewDidLoad()
         
         title = "onboarding"
+        let data = TodoModel(id: 1, title: "James", description: "lets chat", date: "my date", time: "my time", isDone: true)
+//        let data2 = TodoModel(id: 1, title: "James Anyanwu", description: "nil", date: "my date", time: "my time", isDone: false)
+        let todo = TodoConfig()
+//        todo.createTask(task: data)
+//        todo.fetchTask()
+//        todo.createTask(task: data2)
+        todo.fetchTask()
     }
 }
